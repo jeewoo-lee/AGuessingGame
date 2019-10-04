8937522a18ba31191021b3e98a6a7a0a0e5d3e64
Jeewoo Lee <jlee@lcs.on.ca>
2019-10-03 21:11:23 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Lee, Jeewoo on 2019-10-03.
+//  Copyright © 2019 Lee, Jeewoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
